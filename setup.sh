30d2ea98443d034504736fb6abcfc6562b46819b
jhall031 <85783577+jhall031@users.noreply.github.com>
2021-06-26 00:41:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,14 +11,14 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipinfo.io/ip);
-IZIN=$( curl http://54.179.120.207/djtv/ipbaikbanget | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/jhall031/TESTd/main/ipk | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
 echo -e "${red}Permission Denied!${NC}";
 echo "Please Contact Admin"
-echo "Telegram t.me/djas08"
-echo "WhatsApp wa.me/6282381643124"
+echo "Telegram t.me/neko_poii"
+echo "WhatsApp wa.me/62822355501512"
 rm -f setup.sh
 exit 0
 fi
@@ -28,20 +28,20 @@ exit 0
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget http://54.179.120.207/djtv/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/jhall031/TESTd/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget http://54.179.120.207/djtv/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget http://54.179.120.207/djtv/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/jhall031/TESTd/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/jhall031/TESTd/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 #install ssr
-wget http://54.179.120.207/djtv/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget http://54.179.120.207/djtv/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+wget https://raw.githubusercontent.com/jhall031/TESTd/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/jhall031/TESTd/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
 #installwg
-wget http://54.179.120.207/djtv/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/jhall031/TESTd/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
-wget http://54.179.120.207/djtv/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/jhall031/TESTd/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 #install L2TP
-wget http://54.179.120.207/djtv/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-wget http://54.179.120.207/djtv/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+wget https://raw.githubusercontent.com/jhall031/TESTd/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/jhall031/TESTd/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
@@ -66,7 +66,7 @@ WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
-wget -O /etc/set.sh "http://54.179.120.207/djtv/set.sh"
+wget -O /etc/set.sh "https://raw.githubusercontent.com/jhall031/TESTd/main/set.sh"
 chmod +x /etc/set.sh
 history -c
 echo "1.2" > /home/ver
@@ -114,12 +114,12 @@ echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   - Dev/Main                : DjasTv Grup"  | tee -a log-install.txt
-echo "   - Telegram                : T.me/djasri08"  | tee -a log-install.txt
-echo "   - Instagram               : @djas"  | tee -a log-install.txt
-echo "   - Whatsapp                : 082220428038"  | tee -a log-install.txt
-echo "   - Facebook                : https://www.facebook.com/myasri" | tee -a log-install.txt
-echo "------------------Script Created By DjasTV - v2ray Indonesia Grup-----------------" | tee -a log-install.txt
+echo "   - Dev/Main                : JAVpala Community"  | tee -a log-install.txt
+echo "   - Telegram                : T.me/neko_poii"  | tee -a log-install.txt
+echo "   - Instagram               : JAVpala Community"  | tee -a log-install.txt
+echo "   - Whatsapp                : 0822355501512"  | tee -a log-install.txt
+echo "   - Facebook                : JAVpala Community" | tee -a log-install.txt
+echo "------------------Script Created By dKusya - JAVpala-----------------" | tee -a log-install.txt
 echo ""
 echo " Reboot 15 Sec"
 sleep 15
